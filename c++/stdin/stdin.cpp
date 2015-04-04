c9acf75977e9125dfcdb275701e9e3b7e0a59370
knudhansen <knud.hansen@gmail.com>
2015-04-04 18:57:50 +0200

    Adding Makefile. Updating stdin to be able to take input from stdin or from the command line arguments.

--- c++/stdin/stdin.cpp
+++ c++/stdin/stdin.cpp
@@ -1,19 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-  char s[128];
-  int length = 0;
+typedef void (*argument_callback_t)(int argi, char *argv);
 
-  int c;
-  while (EOF != (c = fgetc(stdin))) {
-    if (c == 10) {
-      s[length] = '\0';
-      printf("%s ", s);
-      length = 0;
-    } else {
-      s[length] = c;
-      length++;
+void parse_input(argument_callback_t callback, int argc, char **argv);
+
+void argument_handler(int argi, char *argv) {
+  printf("argument number %d is %s\n", argi, argv);
+}
+
+int main(int argc, char **argv) {
+  parse_input(&argument_handler, argc, argv);
+}
+
+void parse_input(argument_callback_t callback, int argc, char **argv) {
+  /*** arguments from command line ***/
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      callback(i, argv[i]);
     }
+
+  /*** arguments from standard input ***/
+  } else {
+    char s[1024];
+    int length = 0;
+    int i = 1;
+
+    int c;
+    while (EOF != (c = fgetc(stdin))) {
+      if (c == 10 || c == 32) {
+	s[length] = '\0';
+	callback(i, s);
+	i++;
+	length = 0;
+      } else {
+	s[length] = c;
+	length++;
+      }
+    }
+
   }
 }
