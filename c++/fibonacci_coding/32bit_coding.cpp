2705a09be4a2cb746c18428ec14feb7b999f7110
knudhansen <knud.hansen@gmail.com>
2015-04-29 18:04:42 +0200

    cleanup

--- c++/fibonacci_coding/32bit_coding.cpp
+++ c++/fibonacci_coding/32bit_coding.cpp
@@ -2,41 +2,52 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
+void encode_32bits(int a);
+
+int main(int argc, char **argv) {
 
   char s[1024];
   int s_length = 0;
 
   int c;
 
-  while (EOF != (c = fgetc(stdin))) {
-    if (c == 10) {
-      char encoding_32bit[33];
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      encode_32bits(atoi(argv[i]));
+    }
+  } else {
+    while (EOF != (c = fgetc(stdin))) {
+      if (c == 10) {
+	s[s_length] = '\0';
 
-      s[s_length] = '\0';
-      encoding_32bit[32] = '\0';
-
-      int argv_int = atoi(s);
-
-      for (int i = 31; i >=0; i--) {
-	if (argv_int >= pow(2,i)) {
-	  encoding_32bit[i] = '1';
-	  argv_int = argv_int - pow(2,i);
-	} else {
-	  encoding_32bit[i] = '0';
-	}
+	encode_32bits(atoi(s));
+  	
+	s_length = 0;
+	
+      } else {
+	s[s_length] = c;
+	s_length++;
       }
-
-      printf("%s",encoding_32bit);
-
-      s_length = 0;
-
-    } else {
-      s[s_length] = c;
-      s_length++;
     }
   }
 
   printf("\n");
   
+}
+
+void encode_32bits(int a) {
+  char encoding_32bit[33];
+	
+  encoding_32bit[32] = '\0';
+  
+  for (int i = 31; i >=0; i--) {
+    if (a >= pow(2,i)) {
+      encoding_32bit[i] = '1';
+      a = a - pow(2,i);
+    } else {
+      encoding_32bit[i] = '0';
+    }
+  }
+  
+  printf("%s",encoding_32bit);
 }
